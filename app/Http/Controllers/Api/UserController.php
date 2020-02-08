b414e193edb66c4d35adf2fdac974a4733002257
haiou <haiou@localhost.localdomain>
2020-02-08 11:58:35 +0800

    user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -91,7 +91,7 @@ class UserController extends Controller
     {
         $sign = $_GET['sign'];
 //        $sign = 'asdsadsadasdsad';
-        //字典排序
+        //字典排序1
         $data = $_GET['data'];
         print_r($_GET);
 
